1751d6070acb354bbca95f67e9c418ae9cc4140b
Gooosik <163407030+Gooosik@users.noreply.github.com>
2024-03-21 11:59:15 +0200

    Add files via upload

---
+++ Lab6/2.c
@@ -0,0 +1,6 @@
+main()
+{
+    printf("%d\n", 557);
+    printf("%10d\n", 557);
+    printf("%-10d\n", 557);
+}
---
+++ Lab6/5.c
@@ -0,0 +1,6 @@
+#define riadok "Чудова погода"
+int main()
+{
+    printf("%2s\n", riadok);
+    printf("%15.5s\n", riadok); 
+}

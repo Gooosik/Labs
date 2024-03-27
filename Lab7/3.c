7983881807f98267e65660e1fa76b062b5ed1788
Gooosik <163407030+Gooosik@users.noreply.github.com>
2024-03-27 13:27:18 +0200

    3.c

--- Lab7/3.c
+++ Lab7/3.c
@@ -7,8 +7,8 @@ int main() {
     char name[50];
     printf("Як Вас звати?\n"); 
     scanf("%s", name);
-    printf("Привіт, %s. %s\n", name, PRAISE); // Використано макрос PRAISE
-    printf("Ваше ім'я складається з %lu літер і займає %lu комірок пам'яті.\n", strlen(name), sizeof(name)); // Замінено %d на %lu для типу size_t
-    printf("Вигадана фраза складається з %lu літер і займає %lu комірок пам'яті.\n", strlen(PRAISE), sizeof(PRAISE)); // Замінено %d на %lu для типу size_t
+    printf("Привіт, %s. %s\n", name, PRAISE); 
+    printf("Ваше ім'я складається з %lu літер і займає %lu комірок пам'яті.\n", strlen(name), sizeof(name)); 
+    printf("Вигадана фраза складається з %lu літер і займає %lu комірок пам'яті.\n", strlen(PRAISE), sizeof(PRAISE)); 
     return 0;
 }
